c88adf8fd03ad8564be1e33975c714beab37e9e3
tetozi <tozi__@abv.bg>
2022-03-28 18:51:44 +0300

    Add Photo in server

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,8 +1,65 @@
 
 const AppError = require("./../error/appError");
 const catchAsync = require('./../error/catchAsync')
-
 const User = require("../models/userModel");
+const multer = require('multer');
+
+
+const multerStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images/users')
+  },
+  filename: (req, file, cb) => {
+    const ext = file.mimetype.split('/')[1]
+    cb(null, `user-${req.user.id}-${Date.now()}.${ext}`)
+  }
+
+})
+
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image'))
+    cb(null, true)
+  else {
+    cb(new AppError("Not an image!Please upload only a images", 401), false)
+  }
+}
+
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter
+})
+
+exports.uploadUserPhoto = upload.single('photo')
+
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // 1) Create error if user POSTs password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for password updates. Please use /updateMyPassword.',
+        400
+      )
+    );
+  }
+
+  // 2) Filtered out unwanted fields names that are not allowed to be updated
+  const filteredBody = filterObj(req.body, 'name', 'email');
+
+  // 3) Update user document
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser
+    }
+  });
+});
+
 
 
 exports.getOneUser = catchAsync(async (req, res, next) => {
@@ -11,11 +68,11 @@ exports.getOneUser = catchAsync(async (req, res, next) => {
   if (!user) {
     return next(new AppError("Please Log In", 401))
   }
-   
-   res.status(200).json({
-     status: "success",
-     data: {
-       user
-     }
-   });
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      user
+    }
+  });
 })
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const userController = require('../controllers/userController')
+
 
 const authController = require('../controllers/authController')
 const userController = require('../controllers/userController')
+
+
+
 const router = express.Router();
 
 
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
+router.patch('/updateUser', userController.uploadUserPhoto ,userController.updateMe)
 router.get(authController.protect, userController.getOneUser)
 
 module.exports = router;
